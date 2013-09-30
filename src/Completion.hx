0d53873ccfc747130d4ee45f8a8b94fb9b63aaaf
as3boyan <denis.flash.as3@gmail.com>
2013-09-30 11:53:31 +0300

    Anyword completion ported to Haxe.

--- src/Completion.hx
+++ src/Completion.hx
@@ -1,4 +1,6 @@
 package ;
+import CodeMirror;
+import Cursor;
 import haxe.ds.StringMap.StringMap;
 
 /**
@@ -7,14 +9,23 @@ import haxe.ds.StringMap.StringMap;
  */
 class Completion
 {
+	var list:Array<String>;
+	var seen:StringMap<Bool>;
+	var editor:CodeMirror;
+	var word:EReg;
+	var range:Int;
+	var curWord:String;
+	var cur:Cursor;
 
 	public function new() 
 	{
 		var WORD:EReg = ~/[A-Z]+$/i;
 		var RANGE = 500;
 
-		CodeMirror.registerHelper("hint", "haxe", function(editor:CodeMirror, options) {
-			var word:EReg = null;
+		CodeMirror.registerHelper("hint", "haxe", function(current_editor:CodeMirror, options) {
+			editor = current_editor;
+			
+			word = null;
 			
 			if (options != null && options.word != null)
 			{
@@ -25,7 +36,7 @@ class Completion
 				word = WORD;
 			}
 			
-			var range:Int = null;
+			range = null;
 			
 			if (options != null && options.range != null)
 			{
@@ -36,7 +47,7 @@ class Completion
 				range = RANGE;
 			}
 			
-			var cur = editor.getCursor();
+			cur = editor.getCursor();
 			var curLine:String = editor.getLine(cur.line);
 			var start:Int = cur.ch;
 			var end = start;
@@ -44,79 +55,51 @@ class Completion
 			while (end < curLine.length && word.match(curLine.charAt(end))) ++end;
 			while (start > 0 && word.match(curLine.charAt(start - 1))) --start;
 			
-			var curWord = null;
+			curWord = null;
 			
 			if (start != end) 
 			{
 				curWord = curLine.substring(start, end);
 			}
 
-			var list = new Array();
-			var seen:StringMap<Bool> = new StringMap();
+			list = new Array();
+			seen = new StringMap();
 			
-				var dir = -1;
-			
-				var line = cur.line;
-				var end2 = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
-				while (line != end2) 
-				{
-					var text = editor.getLine(line);
-					var m:Dynamic = null;
-					
-					//trace(text);
-					
-					//var re:EReg = ~/[A-Z]+$/i;
-					
-					//
-					//re.map(text, function(e:EReg):String
-					//{
-						//trace(e.matched(0));
-						//return e.matched(0);
-					//}
-					//);
-					
-					//var re:Dynamic = untyped __js__("new RegExp(word.source, \"g\")");
-					
-					//while (re.match(text)) 
-					//{
-						//m = re.matched(0);
-						//re.map
-						//trace(m);
-						//m.push(re.matched(0));
-					
-						//if (line == cur.line && m == curWord) continue;
-						//if ((curWord == null || m.indexOf(curWord) == 0) && !seen.exists(m)) 
-						//{
-							//seen.set(m, true);
-							//list.push(m);
-						//}
-					//}
-					
-					line += dir;
-				}
-			
-			//scan(-1);
-			//scan(1);
+			scan(-1);
+			scan(1);
 			return {list: list, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)};
 		});
 	}
 	
-	//public static function scan(dir)
-	//{
-		//var line = cur.line;
-		//var end = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
-		//while (line != end) {
-		//var text = editor.getLine(line), m;
-		//var re = new RegExp(word.source, "g");
-		//while (m = re.exec(text)) {
-		//if (line == cur.line && m[0] == curWord) continue;
-		//if ((!curWord || m[0].indexOf(curWord) == 0) && !seen.hasOwnProperty(m[0])) {
-		//seen[m[0]] = true;
-		//list.push(m[0]);
-		//}
-		//}
-		//
-		//line += dir;
-		//}
-	//}
+	private function scan(dir:Int):Void
+	{			
+		var line = cur.line;
+		var end2 = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
+		while (line != end2) 
+		{
+			var text = editor.getLine(line);
+			var m:Dynamic = null;
+			
+			var re:EReg = ~/([A-Z]+)/ig;
+			
+			re.map(text, function(e:EReg):String
+			{
+				m = e.matched(0);
+				
+				if (!(line == cur.line) || !(m == curWord))
+				{
+					if ((curWord == null || m.indexOf(curWord) == 0) && !seen.exists(m)) 
+					{
+						seen.set(m, true);
+						list.push(m);
+					}
+				}
+				
+				return e.matched(0);
+			}
+			);
+			
+			line += dir;
+		}
+	}
 }
---
+++ src/Cursor.hx
@@ -0,0 +1,17 @@
+package;
+
+/**
+ * ...
+ * @author AS3Boyan
+ */
+extern class Cursor
+{
+	public var line:Int;
+	public var ch:Int;
+
+	public function new() 
+	{
+		
+	}
+	
+}
