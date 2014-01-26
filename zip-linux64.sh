07fd247d287a2439786b24e3d8cfb3d8a7ae5705
misterpah <misterpah@gmail.com>
2014-01-26 13:39:00 +0800

    latest /dist

--- zip-linux64.sh
+++ zip-linux64.sh
@@ -2,4 +2,4 @@
 cd "`dirname "$0"`"
 rm  ./dist/HIDE-linux64.tar.gz
 #zip -r ./dist/linux64.zip ./ -x /runtime/windows/* -x /runtime/linux-32/* -x /zip-* -x /windows.bat -x /dist/*
-tar -zcvf ./dist/HIDE-linux64.tar.gz ./ --exclude=./runtime/windows --exclude=./dist --exclude=./runtime/linux-32 --exclude=./zip-* --exclude=./windows.bat
+tar -zcvf ./dist/HIDE-linux64.tar.gz ./ --exclude=./runtime/windows --exclude=./dist --exclude=./runtime/linux-32 --exclude=./zip-* --exclude=./windows.bat --exclude=./.git
