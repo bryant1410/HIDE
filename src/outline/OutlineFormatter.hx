5bf08fad5367a481f341424e1b3b905ffb57bf62
Nick Holder <hello@nickholder.co.uk>
2014-06-29 12:26:47 +0100

    Add icons to methods in the outline panel

--- src/outline/OutlineFormatter.hx
+++ src/outline/OutlineFormatter.hx
@@ -1,5 +1,6 @@
 package outline;
 
+import js.html.LIElement;
 import jQuery.JQuery;
 
 /**
@@ -14,22 +15,26 @@ class OutlineFormatter{
 	public function new()
 	{
 		
-		var outlineItems =  untyped new JQuery('#outline').jqxTree('getItems');
+		var outlineItems:Array<Dynamic> =  untyped new JQuery('#outline').jqxTree('getItems');
 		
-		var item:Dynamic;
-		
-		for ( i in 0...outlineItems.length)
+		var li:LIElement;
+
+		for ( item in outlineItems )
 		{
-			untyped item =  outlineItems[i] ;
+			li = cast(item.element, LIElement);
 			
 			if( item.label.split("(").length > 1 )
 			{
-				trace( "found function: " + item.label );
-				trace( item.parentElement.className );
-				item.parentElement.className = item.parentElement.className + " outlineFunction";
-				trace( item.parentElement.className );
+				li.classList.add( "outlineFunction");	
 			}
+			else 
+			{
+				//li.classList.add( "outlineVar");		
+			}
+			
 		}
 
+	
+	
 	}
 }
